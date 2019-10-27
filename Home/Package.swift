7d71b99fb96f8e8a6d08925bbf229bab6e74ae36
Laurent Tréguier <laurent@treguier.org>
2019-10-27 14:05:38 +0100

    Create module Home

---
+++ Home/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Home",
+    products: [
+        .library(
+            name: "Home",
+            targets: ["Home"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Home",
+            dependencies: []),
+    ]
+)
