5972be3c1a745107267439e8759fae53e8ae556c
Laurent Tréguier <laurent@treguier.org>
2019-10-27 14:16:09 +0100

    Create module Archive

---
+++ Archive/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Archive",
+    products: [
+        .library(
+            name: "Archive",
+            targets: ["Archive"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "Archive",
+            dependencies: []),
+    ]
+)
