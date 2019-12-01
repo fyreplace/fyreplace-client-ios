385697773afa1e86e95e570ee5bd0449a18b0e64
Laurent Tréguier <laurent@treguier.org>
2019-12-01 12:27:48 +0100

    Update dependencies

--- Lib/Package.swift
+++ Lib/Package.swift
@@ -12,23 +12,18 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/Swinject/Swinject.git",
-            .upToNextMajor(from: "2.7.1")
-        ),
-        .package(
             url: "https://github.com/ReactiveX/RxSwift.git",
             .upToNextMajor(from: "5.0.1")
         ),
         .package(
             url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git",
-            .upToNextMajor(from: "5.1.1")
+            .upToNextMajor(from: "5.2.0")
         ),
     ],
     targets: [
         .target(
             name: "Lib",
             dependencies: [
-                "Swinject",
                 "RxSwift",
                 "RxCocoa",
                 "RxSwiftExt",
