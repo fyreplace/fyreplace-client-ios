e9bba5aa9ad97398333731825e72d7925a28b344
Laurent Tréguier <laurent@treguier.org>
2020-01-12 15:47:26 +0100

    Use authentication token plugin

--- LibWildFyre/Sources/LibWildFyre/BaseProvider.swift
+++ LibWildFyre/Sources/LibWildFyre/BaseProvider.swift
@@ -4,6 +4,11 @@ import RxMoya
 import RxSwift
 
 public class BaseProvider<T: TargetType>: MoyaProvider<T> {
+    public init() {
+        let tokenPlugin = AccessTokenPlugin { _ in UserDefaults.standard.string(forKey: "auth:token") ?? "" }
+        super.init(plugins: [tokenPlugin])
+    }
+
     public func req<R: Decodable>(_ token: T, as type: R.Type) -> Single<R> {
         rx.request(token).map(type)
     }
