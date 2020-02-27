9e944fabba96b346ff94c17ae9e00cb4f843cb7d
Laurent Tréguier <laurent@treguier.org>
2020-02-28 00:15:02 +0100

    Add missing public modifiers

--- App/Sources/MainViewController.swift
+++ App/Sources/MainViewController.swift
@@ -1,39 +1,39 @@
 import Lib
 import UIKit
 
-class MainViewController: UITabBarController, UITabBarControllerDelegate, CentralDataProvider {
+public class MainViewController: UITabBarController, UITabBarControllerDelegate, CentralDataProvider {
     @IBOutlet
     public var centralViewModel: CentralViewModel!
     @IBOutlet
     private var viewModel: MainViewModel!
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         delegate = self
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    public override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(onDidLogout(_:)), name: .didLogout, object: nil)
     }
 
-    override func viewDidAppear(_ animated: Bool) {
+    public override func viewDidAppear(_ animated: Bool) {
         let notification: NSNotification.Name = viewModel.isLogged ? .didLogin : .didLogout
         let info = viewModel.isLogged ? ["success": true] : nil
         NotificationCenter.default.post(name: notification, object: self, userInfo: info)
     }
 
-    override func viewDidDisappear(_ animated: Bool) {
+    public override func viewDidDisappear(_ animated: Bool) {
         NotificationCenter.default.removeObserver(self)
     }
 
-    override func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
+    public override func overrideTraitCollection(forChild childViewController: UIViewController) -> UITraitCollection? {
         retrieveTraitCollection()
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    public override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         setup(viewController: segue.destination)
     }
 
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+    public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
         setup(viewController: viewController)
         return true
     }
