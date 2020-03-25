979e66ed4b9ada007221cb51d34ef6289276b299
Laurent Tréguier <laurent@treguier.org>
2020-03-25 22:52:27 +0100

    Update dependencies

--- Lib/Package.swift
+++ Lib/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/ReactiveX/RxSwift.git",
-            .upToNextMajor(from: "5.1.0")
+            .upToNextMajor(from: "5.1.1")
         ),
         .package(
             url: "https://github.com/RxSwiftCommunity/RxSwiftExt.git",
@@ -24,7 +24,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/SDWebImage/SDWebImage.git",
-            .upToNextMajor(from: "5.6.0")
+            .upToNextMajor(from: "5.6.1")
         ),
         .package(
             url: "https://github.com/SDWebImage/SDWebImageWebPCoder.git",
